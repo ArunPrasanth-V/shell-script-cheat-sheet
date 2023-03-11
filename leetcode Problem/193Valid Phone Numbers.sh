dd7c7420d2999ef73ee1101bf8c6a1a1f7936c77
Arun Prasanth <88626504+ArunPrasanth-V@users.noreply.github.com>
2023-03-12 01:52:26 +0530

    Create

---
+++ leetcode Problem/193Valid Phone Numbers.sh
@@ -0,0 +1,3 @@
+#  193. Valid Phone Numbers
+#  https://leetcode.com/problems/valid-phone-numbers/description/
+ sed -n '/^\(\([[:digit:]]\)\{3\}-\|(\([[:digit:]]\)\{3\}) \)\([[:digit:]]\)\{3\}[-]\{1\}\([[:digit:]]\)\{4\}$/p' file.txt
