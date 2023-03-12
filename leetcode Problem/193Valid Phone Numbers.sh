186bec803dbb0fcba4da22009827921e4f20205c
Arun Prasanth <88626504+ArunPrasanth-V@users.noreply.github.com>
2023-03-12 10:36:46 +0530

    Update 193Valid Phone Numbers.sh

--- leetcode Problem/193Valid Phone Numbers.sh
+++ leetcode Problem/193Valid Phone Numbers.sh
@@ -1,5 +1,6 @@
 #  193. Valid Phone Numbers
 #  https://leetcode.com/problems/valid-phone-numbers/description/
- sed -n '/^\(\([[:digit:]]\)\{3\}-\|(\([[:digit:]]\)\{3\}) \)\([[:digit:]]\)\{3\}[-]\{1\}\([[:digit:]]\)\{4\}$/p' file.txt
-
  sed -n '/^\([[:digit:]]\{3\}-\|([[:digit:]]\{3\}) \)[[:digit:]]\{3\}-[[:digit:]]\{4\}$/p' file.txt #optmized one
+ 
+ grep -Po '^(\(\d{3}\) |\d{3}-)\d{3}-\d{4}$' file.txt
+#-P Perl Compatible Regular Expressions (PCRE) is a regular expression library that provides a powerful and flexible set of pattern-matching tools for text processing.
