8cabf2a43b9a632b2f4e88116d1a6dfe72ffcb2b
Arun Prasanth <88626504+ArunPrasanth-V@users.noreply.github.com>
2023-03-12 09:37:28 +0530

    Update 193Valid Phone Numbers.sh

--- leetcode Problem/193Valid Phone Numbers.sh
+++ leetcode Problem/193Valid Phone Numbers.sh
@@ -1,3 +1,5 @@
 #  193. Valid Phone Numbers
 #  https://leetcode.com/problems/valid-phone-numbers/description/
  sed -n '/^\(\([[:digit:]]\)\{3\}-\|(\([[:digit:]]\)\{3\}) \)\([[:digit:]]\)\{3\}[-]\{1\}\([[:digit:]]\)\{4\}$/p' file.txt
+
+ sed -n '/^\([[:digit:]]\{3\}-\|([[:digit:]]\{3\}) \)[[:digit:]]\{3\}-[[:digit:]]\{4\}$/p' file.txt #optmized one
